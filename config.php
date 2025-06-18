537c596889805656a698b8cee18f3b3bd9fdb703
neveront <memberty.asis@gmail.com>
2025-06-18 23:31:38 +0800

    changed the database name

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','','coffee_shop') or die('connection failed');
 
 ?>
